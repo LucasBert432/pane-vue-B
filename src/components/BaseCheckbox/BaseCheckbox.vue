2dc1e77a36789ca845eb8b6f12a607d6dc5b3f5e
lucas <lucasroberto432@gmail.com>
2026-01-31 21:27:08 -0300

    feat: creating component BaseCheckbox

---
+++ src/components/BaseCheckbox/BaseCheckbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <label :class="$style.wrapper">
+    <input
+      type="checkbox"
+      :checked="modelValue"
+      @change="$emit('update:modelValue', $event.target.checked)"
+    />
+    <span><slot></slot></span>
+  </label>
+</template>
+
+<script setup>
+defineProps({ modelValue: Boolean });
+</script>
+
+<style module lang="scss">
+.wrapper {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 14px;
+  margin-top: 16px;
+}
+</style>
